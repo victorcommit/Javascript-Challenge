ab6ea363407010a55c46dac1cd4ab37aae7ec0b3
victorcommit <alvinvictor@dyn-160-39-224-238.dyn.columbia.edu>
2020-02-13 21:02:16 -0500

    update

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -39,12 +39,10 @@ function handleClick() {
   }
 
   // Rebuild the table using the filtered data
-  // @NOTE: If no date was entered, then filteredData will
-  // just be the original tableData.
   buildTable(filteredData);
 }
 
-// Attach an event to listen for the form button
+// Attached an event to listen for the form button
 d3.selectAll("#filter-btn").on("click", handleClick);
 
 // Build the table when the page loads
